0615fe7cb9f87fa6c67d1d728e6405a6c7f2e68e
Levmas25 <93665133+Levmas25@users.noreply.github.com>
2021-12-24 19:35:10 +0300

    Add files via upload

---
+++ WPF/дз1/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            foreach (UIElement el in grid.Children)
+            {
+                if (el is Button)
+                {
+                    ((Button)el).Click += Button_Click;
+                }
+            }
+            pwBox.PasswordChanged += PasswordChangedGandler;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(pwBox.Password.Length < 4)
+                pwBox.Password += (string)((Button)sender).Content;
+
+        }
+        private async void PasswordChangedGandler(object sender, RoutedEventArgs args)
+        {
+            if (pwBox.Password.Length >= 4)
+            {
+                if (pwBox.Password == "1234")
+                {
+                    pwBox.Password = "";
+                    textBox.Background = new SolidColorBrush(Color.FromRgb(62, 229, 47));
+                    textBox.Visibility = Visibility;
+                    textBox.Text = "Access!";
+                    await Task.Delay(1200);
+                    grid.Children.Remove(textBox);
+                    grid.Children.Remove(pwBox);
+                }
+                else
+                {
+                    pwBox.Password = "";
+                    textBox.Background = new SolidColorBrush(Color.FromRgb(229, 86, 47));
+                    textBox.Visibility = Visibility;
+                    textBox.Text = "Wrong!";
+                    await Task.Delay(1200);
+                    textBox.Visibility = Visibility.Hidden;
+                }
+            }
+        }
+    }
+}
---
+++ WPF/дз2/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random random = new Random();
+        DispatcherTimer t = new DispatcherTimer();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            t.Interval = new TimeSpan(0, 0, 5);
+            t.Tick += Ontime;
+            t.Start();
+            foreach(UIElement el in some.Children)
+            {
+                if(el is Button)
+                {
+                    ((Button)el).Click += Button_Click;
+                }
+            }
+            textBox.TextChanged += Text_Changed;
+        }
+
+        private void Ontime(object sender, EventArgs e)
+        {
+            for(int i = 0; i < some.Children.Count - 1; i++)
+            {
+                if ((some.Children[i] is Button) & (some.Children[i + 1] is Button))
+                {
+                    var margin = ((Button)some.Children[i]).Margin;
+                    ((Button)some.Children[i]).Margin = ((Button)some.Children[i + 1]).Margin;
+                    ((Button)some.Children[i + 1]).Margin = margin;
+                    SolidColorBrush background = new SolidColorBrush();
+                    background.Color = Color.FromRgb(Convert.ToByte(random.Next(0, 255)), Convert.ToByte(random.Next(0, 255)), Convert.ToByte(random.Next(0, 255)));
+                    ((Button)some.Children[i]).Background = background;
+                    SolidColorBrush brush = new SolidColorBrush();
+                    brush.Color = Color.FromRgb(Convert.ToByte(random.Next(0, 255)), Convert.ToByte(random.Next(0, 255)), Convert.ToByte(random.Next(0, 255)));
+                    zero.Background = brush;
+                    nine.Background = brush;
+                }
+            }
+        }
+        private void Button_Click(object sender, RoutedEventArgs args)
+        {
+            if ((string)((Button)sender).Content == "⛌")
+            {
+                foreach (UIElement el in some.Children)
+                {
+                    if (el is Button)
+                        ((Button)el).Content = "";
+                }
+            }
+            else
+            {
+                for (int i = 0; i < some.Children.Count - 1; i++)
+                {
+                    if ((some.Children[i] == (Button)sender) & (some.Children[i + 1] is Button))
+                    {
+                        var margin = ((Button)some.Children[i]).Margin;
+                        ((Button)some.Children[i]).Margin = ((Button)some.Children[i + 1]).Margin;
+                        ((Button)some.Children[i + 1]).Margin = margin;
+                        if (textBox.Text.Length < 5)
+                        {
+                            textBox.Text += ((Button)some.Children[i + 1]).Content;
+                            textBox.Margin = new Thickness(random.Next(250), random.Next(200), 0, 0);
+                        }
+                        break;
+                    }
+                }
+            }
+
+        }
+        private void Text_Changed(object sender, RoutedEventArgs args)
+        {
+            if(textBox.Text.Length == 5)
+            {
+                textBox.Background = new SolidColorBrush(Color.FromRgb(62, 229, 47));
+                textBox.Text = "Access";
+            }
+        }
+    }
+}
